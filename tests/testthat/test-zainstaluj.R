c0cdfd1e5f7d465c3c350d179bfa5d70ce612efe
Mateusz Żółtak <zozlak@zozlak.org>
2015-04-18 15:43:56 +0200

    Eksperymentów z R check pod Travis-em c.d.

--- tests/testthat/test-zainstaluj.R
+++ tests/testthat/test-zainstaluj.R
@@ -3,7 +3,7 @@ context('zainstaluj')
 test_that('zainstaluj dziala', {
   katalog = getwd()
   expect_equal(katalog, 'aaa')
-  setwd(sub('MLAK.Rcheck$', 'MLAK', sub('/tests/testthat$', '', katalog)))
+  setwd(sub('MLAK.Rcheck$', '', sub('/tests/testthat$', '', katalog)))
   expect_equal(zainstaluj(), TRUE)
   setwd(katalog)
 })
