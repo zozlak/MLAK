6e8107e6abef15f8d4421f4d406dcf419f130009
Mateusz Żółtak <zozlak@zozlak.org>
2015-04-18 19:37:00 +0200

    Zwiększanie pokrycia testami

--- inst/raporty/R2-uczelnia/R2-uczelnia.R
+++
@@ -1,9 +0,0 @@
-library(MLAK)
-
-generujRaporty(
-  plikSzablonu   = 'inst/raporty/R2-uczelnia/R2-uczelnia.Rmd', 
-  dane           = 'inst/raporty/R2-uczelnia/dane.csv',
-  grupyOdbiorcow = 'inst/raporty/R2-uczelnia/odbiorcy.csv',
-  katalogWy      = 'raporty', 
-  prefiksPlikow  = 'R2-'
-)
--- inst/raporty/R3k-dziekan/R3k-dziekan.R
+++
@@ -1,9 +0,0 @@
-library(MLAK)
-
-generujRaporty(
-  plikSzablonu   = 'raporty/R3k-dziekan/R3k-dziekan.Rmd', 
-  dane           = 'raporty/R3k-dziekan/dane.csv',
-  grupyOdbiorcow = 'raporty/R3k-dziekan/odbiorcy.csv',
-  katalogWy      = 'raporty', 
-  prefiksPlikow  = 'R3k-'
-)
