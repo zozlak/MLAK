f0594df9923b945a872e36744c56dd4e64230e1e
Mateusz Żółtak <zozlak@zozlak.org>
2015-04-18 18:40:19 +0200

    Zwiększanie pokrycia testami

--- tests/testthat/test-zainstaluj.R
+++ tests/testthat/test-zainstaluj.R
@@ -2,8 +2,6 @@ context('zainstaluj')
 
 test_that('zainstaluj dziala', {
 #  katalog = getwd()
-  #/home/travis/build/zozlak/MLAK/MLAK.Rcheck/tests/testthat
-  #/home/travis/build/zozlak/MLAK/
 #  setwd(sub('MLAK.Rcheck$', '', sub('/tests/testthat$', '', katalog)))
 #  expect_equal(zainstaluj(), TRUE)
 #  setwd(katalog)
