4293aa19cc275a01549f9978296746d0598724d6
Mateusz Żółtak <zozlak@zozlak.org>
2015-04-18 15:29:51 +0200

    Eksperymenty z testami po stronie Travis-a

--- tests/testthat/test-zainstaluj.R
+++ tests/testthat/test-zainstaluj.R
@@ -2,6 +2,7 @@ context('zainstaluj')
 
 test_that('zainstaluj dziala', {
   katalog = getwd()
+  expect_equal(katalog, 'aaa')
   setwd(sub('MLAK.Rcheck$', 'MLAK', sub('/tests/testthat$', '', katalog)))
   expect_equal(zainstaluj(), TRUE)
   setwd(katalog)
