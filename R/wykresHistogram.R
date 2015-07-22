87e3624b68992a6d939b8f0f83ffc1f376cfa481
Mateusz Żółtak <zozlak@zozlak.org>
2015-07-22 11:08:56 +0200

    Walka z histogramem, c.d.

--- R/wykresHistogram.R
+++ R/wykresHistogram.R
@@ -20,15 +20,7 @@ wykresHistogram = function(dane, n = 9, tytul = '', tytulX = NULL, tytulY = NULL
   )
   
   dane = na.exclude(dane)
-  
-  wykres = ggplot(data = data.frame(d = dane))
-  if(rownePrzedzialy){
-    wykres = wykres + aes(x = get('d'))
-  }else{
-    wykres = wykres + aes(x = get('d'), y = ..density..)
-  }
     
-  
   if(is.numeric(dane)){
     if(rownePrzedzialy){
       breaks = seq(min(dane), max(dane), length.out = n + 1)
@@ -39,18 +31,33 @@ wykresHistogram = function(dane, n = 9, tytul = '', tytulX = NULL, tytulY = NULL
         n = n - 1
         breaks = quantile(dane, seq(0, 1, length.out = n + 1))
       }
+      # straszliwe ręczne generowanie wysokości słupków
+      szer = cbind(breaks[-1], breaks[-length(breaks)])
+      szer = 1 / (szer[, 1] - szer[, 2])
+      szer = szer / sum(szer)
+      breaksTmp = breaks + 10^-6
+      breaksTmp[1] = breaks[1]
+      grupy = cut(dane, breaksTmp, labels = names(szer), right = FALSE)
+      wagi = szer[grupy]
+      wys = aggregate(wagi, list(names(wagi)), function(x){x[1] * length(x)})
+      wys$x = round(10 * wys$x)
+      srednie = rowMeans(cbind(breaks[-1], breaks[-length(breaks)]))
+      wys$grupa = srednie[wys[, 1]] - 1 #!!!
+      dane = as.numeric(unlist(by(wys, wys$grupa, function(x){
+        return(rep(x$grupa[1], x$x[1]))
+      })))
     }
 
-    wykres = wykres +
+    wykres = ggplot(data = data.frame(d = dane)) + aes(x = get('d')) +
       suppressWarnings( # różna szerokość słupków generuje "position_stack requires constant width: output may be incorrect"
         geom_histogram(
           breaks = breaks,
           colour = '#000000',
           fill = '#FFFFFF'
-        )
+        ) 
       )
   }else{
-    wykres = wykres +
+    wykres = ggplot(data = data.frame(d = dane)) + aes(x = get('d')) +
       geom_histogram(
         colour = '#000000',
         fill = '#FFFFFF'
