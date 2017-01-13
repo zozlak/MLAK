d7d907e7cbde46b671e01b7a5de76c9fb9f66dc0
zozlak <zozlak@zozlak.org>
2017-01-13 08:15:21 +0100

    Wyśrodkowanie tytułów wykresów.

--- R/wykresDefaultTheme.R
+++ R/wykresDefaultTheme.R
@@ -8,7 +8,7 @@
 #' @param tytulY tytuł osi Y wykresu
 #' @param rozmiarTekstu domyślny rozmiar tekstu
 wykresDefaultTheme = function(wykres, tytul = NULL, tytulX = NULL, tytulY = NULL, rozmiarTekstu = NULL){
-  if(is.null(rozmiarTekstu)){
+  if (is.null(rozmiarTekstu)) {
     rozmiarTekstu = 10
   }
   
@@ -19,23 +19,23 @@ wykresDefaultTheme = function(wykres, tytul = NULL, tytulX = NULL, tytulY = NULL
     ggplot2::labs(fill = '') +
     ggplot2::theme_bw(base_size = rozmiarTekstu) + 
     ggplot2::theme(
-      panel.border      = element_rect(fill = NA, colour=NA),
+      panel.border      = ggplot2::element_rect(fill = NA, colour = NA),
       legend.position   = 'right',
-      legend.text       = element_text(size = rozmiarTekstu),
-      axis.text.x       = element_text(size = rozmiarTekstu),
-      axis.text.y       = element_text(size = rozmiarTekstu),
-      axis.title.x      = element_text(size = rozmiarTekstu * 1.2),
-      axis.title.y      = element_text(size = rozmiarTekstu * 1.2),
-      plot.title        = element_text(size = rozmiarTekstu * 1.3)
+      legend.text       = ggplot2::element_text(size = rozmiarTekstu),
+      axis.text.x       = ggplot2::element_text(size = rozmiarTekstu),
+      axis.text.y       = ggplot2::element_text(size = rozmiarTekstu),
+      axis.title.x      = ggplot2::element_text(size = rozmiarTekstu * 1.2),
+      axis.title.y      = ggplot2::element_text(size = rozmiarTekstu * 1.2),
+      plot.title        = ggplot2::element_text(size = rozmiarTekstu * 1.3, hjust = 0.5)
     )
   
-  if(!is.null(tytul)){
+  if (!is.null(tytul)) {
     wykres = wykres + ggtitle(tytul)  
   }
-  if(!is.null(tytulX)){
+  if (!is.null(tytulX)) {
     wykres = wykres + xlab(tytulX)
   }
-  if(!is.null(tytulY)){
+  if (!is.null(tytulY)) {
     wykres = wykres + ylab(tytulY)
   }
   
