376337a333085fb1c00af357da21ef1bf16c3321
Mateusz Żółtak <zozlak@zozlak.org>
2018-04-25 21:27:56 +0200

    rozpoznawanie pozycji w stosie wywołań w ustawWyrownaj() zaktualizowane do obecnej wersji pakietów knitr/rmarkdown

--- R/ustawWyrownaj.R
+++ R/ustawWyrownaj.R
@@ -15,12 +15,12 @@ ustawWyrownaj = function(wyrownaj){
   stopifnot(
     is.vector(wyrownaj), is.logical(wyrownaj), length(wyrownaj) == 1
   )
-  if(is.na(wyrownaj)){
+  if (is.na(wyrownaj)) {
     wyrownaj = FALSE
     stos = paste0(sys.calls())
     pozInline = length(stos) - suppressWarnings(max(which(grepl('^process_group.inline[(]', stos))))
     pozBlock  = length(stos) - suppressWarnings(max(which(grepl('^process_group.block[(]', stos))))
-    if(pozInline == 8 | pozBlock == 12){
+    if (pozInline == 9 | pozBlock == 14) {
       wyrownaj = TRUE
     }
   }
